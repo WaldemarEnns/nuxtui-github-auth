0662b591a9bcc52d24b9b1ee68b6a23f2c1f77c8
Waldemar Enns <ennsdesigns96@gmail.com>
2025-02-01 09:13:17 +0100

    📦 Installs test-environment
    Vitest, Nuxt & Vue test-utils, JSDOM, Coverage-c8

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
+/// <reference types="vitest" />
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', 'nuxt-auth-utils'],
+  ignore: ['**/*.spec.ts'],
+  modules: [
+    '@nuxt/ui',
+    'nuxt-auth-utils',
+    '@nuxt/test-utils/module'
+  ]
 })
