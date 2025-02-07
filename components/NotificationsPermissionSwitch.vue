ab33dcfd9aedf91bbf1bd695a05537b4aaf72346
Waldemar Enns <ennsdesigns96@gmail.com>
2025-02-07 09:03:57 +0100

    Adds NotificationsPermissionSwitch.vue component
    Responsible for setting the users permission for browser notifications.

---
+++ components/NotificationsPermissionSwitch.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const notificationsPermission = computed(() => {
+  if (!'Notification' in window) {
+    console.debug('Notifications are not supported in this browser')
+    return false
+  }
+
+  return Notification.permission === 'granted'
+})
+
+function requestNotificationsPermission () {
+  console.debug('Requesting notifications permission')
+  if (Notification.permission === 'granted') {
+    return;
+  }
+
+  Notification.requestPermission((permission) => {
+    console.debug('Permission:', permission)
+    if (permission === 'granted') {
+      new Notification('You have granted permission to receive notifications');
+    }
+  })
+}
+
+function sendTestNotification () {
+  console.debug('Sending test notification')
+  new Notification('This is a test notification');
+}
+</script>
+
+<template>
+  <ClientOnly>
+    <UToggle
+      color="primary"
+      @click="requestNotificationsPermission"
+      :model-value="notificationsPermission"
+      :disabled="notificationsPermission"
+      on-icon="i-heroicons-check-20-solid"
+      off-icon="i-heroicons-x-mark-20-solid"
+    >
+      {{ notificationsPermission ? 'Disable' : 'Enable' }} notifications
+    </UToggle>
+
+    <UButton
+      v-if="notificationsPermission"
+      @click="sendTestNotification"
+      color="primary"
+      class="ml-2"
+    >
+      Send test notification
+    </UButton>
+  </ClientOnly>
+</template>
