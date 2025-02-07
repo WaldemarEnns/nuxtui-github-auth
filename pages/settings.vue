b317ed0cf841ae37f75d3d93eb487ddab73acb4e
Waldemar Enns <ennsdesigns96@gmail.com>
2025-02-07 09:04:03 +0100

    Adds content to settings page

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,3 +1,21 @@
 <template>
-  <h1>Settings</h1>
+  <UCard>
+    <template #header>
+      <h1>Settings</h1>
+    </template>
+
+    <UFormGroup label="Theme" class="mb-4">
+      <div class="flex flex-row justify-start items-center">
+        <ThemeSwitch />
+        <span class="ml-2">Switch your theme</span>
+      </div>
+    </UFormGroup>
+
+    <UFormGroup label="Notifications" name="notifications">
+      <div class="flex flex-row justify-start items-center">
+        <NotificationsPermissionSwitch />
+        <span class="ml-2">Enable or disable notifications</span>
+      </div>
+    </UFormGroup>
+  </UCard>
 </template>
