01ed9c60f8a765c03a5248d68bf42c4396e4c201
Waldemar Enns <ennsdesigns96@gmail.com>
2025-02-08 10:27:41 +0100

    Upserts user when successfully authenticated

--- server/routes/auth/github.get.ts
+++ server/routes/auth/github.get.ts
@@ -1,13 +1,30 @@
+import prisma from '~/lib/prisma'
+
 export default defineOAuthGitHubEventHandler({
   config: {
     emailRequired: true
   },
   async onSuccess(event, { user, tokens }) {
+    const databaseUser = await prisma.user.upsert({
+      where: { email: user.email, auth_provider: 'github', auth_provider_user_id: user.id },
+      update: {
+        full_name: user.name,
+        avatar_url: user.avatar_url
+      },
+      create: {
+        email: user.email,
+        avatar_url: user.avatar_url,
+        full_name: user.name,
+        auth_provider: 'github',
+        auth_provider_user_id: user.id
+      }
+    })
+
     await setUserSession(event, {
       user: {
-        avatar_url: user.avatar_url ?? null,
-        email: user.email,
-        name: user.name,
+        avatar_url: databaseUser.avatar_url ?? null,
+        email: databaseUser.email,
+        name: databaseUser.full_name,
         url: user.url,
       }
     })
