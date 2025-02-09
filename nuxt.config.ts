61647dade5f6affcedfafcc5a2e664e64e461cdb
Waldemar Enns <ennsdesigns96@gmail.com>
2025-02-09 01:07:15 +0100

    Adds nuxt-mail and configuration
    Allows sending mails.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,6 +9,21 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     'nuxt-auth-utils',
     '@nuxt/test-utils/module',
-    '@prisma/nuxt'
-  ]
+    '@prisma/nuxt',
+    ['nuxt-mail', {
+      smtp: {
+        host: process.env.MAIL_SMTP_HOST,
+        port: process.env.MAIL_SMTP_PORT,
+        auth: {
+          user: process.env.MAIL_SMTP_USER,
+          pass: process.env.MAIL_SMTP_PASSWORD,
+        },
+        secure: false,
+      },
+      message: {
+        from: process.env.MAIL_FROM_ADDRESS,
+        cc: process.env.MAIL_FROM_ADDRESS,
+      }
+    }],
+  ],
 })
