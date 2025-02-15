e4fa827657c66502d94bcff03d5d83db35c2b3ec
Waldemar Enns <ennsdesigns96@gmail.com>
2025-02-15 11:23:24 +0100

    ♻️ Refactor teams management page query
    Simplifies database query for fetching user's owned teams by removing unnecessary variable assignments and chaining the query more directly

--- pages/teams-management.server.vue
+++ pages/teams-management.server.vue
@@ -3,17 +3,10 @@ import type { Team } from '@prisma/client'
 const prisma = usePrismaClient()
 const { user } = useUserSession()
 
-const userId = user.value!.id
-const dbUser = await prisma.user.findUnique({
-  where: {
-    id: userId
-  },
-  include: {
-    ownedTeams: true
-  }
-})
-
-const ownedTeams: Team[] = dbUser!.ownedTeams
+const usersTeams = await prisma.user.findUnique({
+  where: { id: user.value!.id },
+  include: { ownedTeams: true }
+})!.ownedTeams
 </script>
 
 <template>
@@ -21,7 +14,7 @@ const ownedTeams: Team[] = dbUser!.ownedTeams
   <div>
     <h2>Owned teams</h2>
     <ul>
-      <li v-for="team in ownedTeams" :key="team.id">
+      <li v-for="team in usersTeams" :key="team.id">
         <NuxtLink :to="`/teams/${team.id}`">
           {{ team.name }}
         </NuxtLink>
